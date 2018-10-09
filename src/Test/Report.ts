1c15e977539c66d187a339d0089801912ac4129c
arthurKuro <arthur.geneau@gmail.com>
2018-10-09 19:54:57 -0400

    basic test

---
+++ src/Test/Report.ts
@@ -0,0 +1,63 @@
+export class Report {
+
+    public Equals() {
+
+    }
+
+    public DeepEquals() {
+
+    }
+
+    public StrictEquals() {
+
+    }
+
+    public DeepStrictEquals() {
+
+    }
+
+    public DoesNotReject() {
+
+    }
+
+    public DoesNotThrow() {
+
+    }
+
+    public Fails() {
+
+    }
+
+    public IfError() {
+
+    }
+
+    public NotEquals() {
+
+    }
+
+    public NotDeepEquals() {
+
+    }
+
+    public NotStrictEquals() {
+
+    }
+
+    public NotDeepStrictEquals() {
+
+    }
+
+    public True() {
+
+    }
+
+    public Rejects() {
+
+    }
+
+    public Throws() {
+        
+    }
+
+}
---
+++ src/Test/Test.ts
@@ -0,0 +1,62 @@
+import * as assert from "assert";
+
+class TestCase {
+
+    constructor(private name: string) {}
+
+    public GetName() {
+        return this.name;
+    }
+
+    public StrictEquals(actual: any, expected: any, message?: string|Error) {
+        assert.strictEqual(actual, expected, message);
+    }
+
+    public DeepStrictEquals(actual: any, expected: any, message?: string|Error) {
+        assert.deepStrictEqual(actual, expected, message);
+    }
+
+    public DoesNotReject(block: Function|Promise<any>, message?: string|Error) {
+        assert.doesNotReject(block, message);
+    }
+
+    public DoesNotThrow(block: Function, message?: string|Error) {
+        assert.doesNotThrow(block, message);
+    }
+
+    public Fail(message?: string|Error) {
+        assert.fail(message);
+    }
+
+    public IfError(value: any) {
+        assert.ifError(value);
+    }
+
+    public NotStrictEquals(actual: any, expected: any, message?: string|Error) {
+        assert.notStrictEqual(actual, expected, message);
+    }
+
+    public NotDeepStrictEquals(actual: any, expected: any, message?: string|Error) {
+        assert.notDeepStrictEqual(actual, expected, message);
+    }
+
+    public True(value: any, message?: string|Error) {
+        assert.ok(value, message)
+    }
+
+    public Rejects(block: Function|Promise<any>, message?: string|Error) {
+        assert.rejects(block, message);
+    }
+
+    public Throws(block: Function, message?: string|Error) {
+        assert.throws(block, message);
+    }
+
+}
+
+let Test = (testName: string, test: (test: TestCase) => void) => {
+    let t = new TestCase(testName);
+    test(t);
+}
+
+export { Test };
