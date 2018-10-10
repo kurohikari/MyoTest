59dc8c0b7f13407af5eb4d13b78ba47f029ff9cf
arthurKuro <arthur.geneau@gmail.com>
2018-10-09 21:42:38 -0400

    added basic main.js

---
+++ main.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+const reserved = ["-s", "-o"];
+
+function main() {
+    let args = process.argv;
+    let source = null;
+    let output = null;
+    for(let i = 0; i<args.length; i++) {
+        let arg = args[i];
+        if(arg === "-s") {
+            source = GetSource(args, i);
+        } else if(arg === "-o") {
+            output = GetOutput(args, i);
+        }
+    }
+    if(source === null) {
+        throw new Error("No argument passed for test source directory");
+    } else if(output === null) {
+        throw new Error("No argument passed for test output directory");
+    } else {
+        console.log("All okay!");
+    }
+}
+
+function GetSource(args, index) {
+    if(args.length <= index+1) {
+        throw new Error("No argument passed for test source directory");
+    }
+    let next = args[index+1];
+    if(reserved.indexOf(next) > -1) {
+        throw new Error("No argument passed for test source directory");
+    }
+    return next;
+}
+
+function GetOutput(args, index) {
+    if(args.length <= index+1) {
+        throw new Error("No argument passed for test output directory");
+    }
+    let next = args[index+1];
+    if(reserved.indexOf(next) > -1) {
+        throw new Error("No argument passed for test output directory");
+    }
+    return next;
+}
+
+main();
