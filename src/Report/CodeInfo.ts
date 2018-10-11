efb0143008846c0ca2b9572976fb661ade61e092
arthurKuro <arthur.geneau@gmail.com>
2018-10-11 12:50:36 -0400

    commented CodeInfo.ts

--- src/Report/CodeInfo.ts
+++ src/Report/CodeInfo.ts
@@ -13,6 +13,9 @@ export class CodeInfo {
         this.RetrieveCodeLine();
     }
 
+    /**
+     * Find the specific stack line corresponding to the test that succeeded
+     */
     private RetrieveStackLine() {
         for(let line of this.stackLines) {
             if(line.indexOf(this.file) > -1) {
@@ -22,6 +25,9 @@ export class CodeInfo {
         }
     }
 
+    /**
+     * Find the path and line number of the code for the test that succeeded form the stack line
+     */
     private RetrievePathAndLineNumber() {
         let pathnum = this.stackLine.substring(this.stackLine.indexOf("(")+1, this.stackLine.lastIndexOf(")"));
         let linecolumn = pathnum.match(/(:\d+:\d+)/)[0];
@@ -29,16 +35,39 @@ export class CodeInfo {
         this.lineNumber = parseInt(linecolumn.split(":")[1])-1;
     }
 
+    /**
+     * Find the line of the code containing the test statement in the file of the test
+     */
     private RetrieveCodeLine() {
         this.codeLine = fs.readFileSync(this.path).toString().split("\n")[this.lineNumber].trim();
     }
 
+    /**
+     * Get the stakline corresponding to the successful test
+     */
+    public GetStackLine() {
+        return this.stackLine;
+    }
+
+    /**
+     * Get the line of code with the test that succeeded
+     */
     public GetCodeLine() {
         return this.codeLine;
     }
 
+    /**
+     * Get the line number in the code of the line containing the test
+     */
     public GetLine() {
         return this.lineNumber;
+    }
+
+    /**
+     * The the file name of the file containing the test
+     */
+    public GetFile() {
+        return this.file;
     }
 
 }
