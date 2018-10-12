f42710b49acbb45a51819d6c564de60148fc1bf4
arthurKuro <arthur.geneau@gmail.com>
2018-10-11 23:05:02 -0400

    updated version

--- src/Test/Test.js
+++ src/Test/Test.js
@@ -158,10 +158,10 @@ let Test = (testName, test) => {
         console.log(`[${t.GetName()}] ${JSON.stringify(t.GetInfo())}`);
     }
     catch (error) {
-        let info = error;
-        info.stackMessage = info.stack;
-        info.errorMessage = info.message;
-        console.error(`[${t.GetName()}] ${JSON.stringify(info)}`);
+        let err = error;
+        err.stackMessage = err.stack;
+        err.errorMessage = err.message;
+        console.error(`[${t.GetName()}] ${JSON.stringify({ "info": t.GetInfo(), "err": err })}`);
     }
 };
 exports.Test = Test;
