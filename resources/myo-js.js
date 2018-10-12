c6fd9ba82143220638e22c87ef768f26c020ce07
arthurKuro <arthur.geneau@gmail.com>
2018-10-12 00:48:53 -0400

    implemented js functions

--- resources/myo-js.js
+++ resources/myo-js.js
@@ -12,20 +12,36 @@ function closeNav() {
     //document.getElementById("main").style.marginLeft = "0";
 }
 
+/* Shows the failures divs and hides the successes. Shows all if Successes are already hidden */
 function ShowFailures() {
-
+    let oks = document.querySelectorAll("div.ok-test");
+    let kos = document.querySelectorAll("ko-test");
+    // Checks if successes are already hidden
+    if(oks[1].style.height === "0px") ShowAll();
+    else {
+        for(let ok of oks) ok.style.height = "0px";
+        for(let ko of kos) ko.style.height = "auto";
+    }
 }
 
+/* Shows the successes divs and hides the failures. Shows all if failures are already hidden */
 function ShowSuccesses() {
-
+    let oks = document.querySelectorAll("div.ok-test");
+    let kos = document.querySelectorAll("ko-test");
+    // Checks if successes are already hidden
+    if(kos[1].style.height === "0px") ShowAll();
+    else {
+        for(let ok of oks) ok.style.height = "auto";
+        for(let ko of kos) ko.style.height = "0px";
+    }
 }
 
-function HideFailures() {
-
-}
-
-function HideSuccesses() {
-    
+/* Shows all failures and successes */
+function ShowAll() {
+    let oks = document.querySelectorAll("div.ok-test");
+    let kos = document.querySelectorAll("ko-test");
+    for(let ok of oks) ok.style.height = "auto";
+    for(let ko of kos) ko.style.height = "auto";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
